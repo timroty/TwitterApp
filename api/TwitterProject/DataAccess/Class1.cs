02225bdb0c8e4daa084a0eb33c1466225cecdcf8
timroty <timroty13@gmail.com>
2019-11-10 23:39:23 -0600

    Update Naming Scheme

--- api/TwitterProject/DataAccess/Class1.cs
+++ api/TwitterProject/DataAccess/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tweetinvi;
 
 namespace DataAccess
 {
     public class Class1
     {
+      
     }
 }
